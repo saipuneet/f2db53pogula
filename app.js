93ca1285d4e0d7b209eb53438f020c1fd4d26110
saipuneet <saipuneethpogula7@gmail.com>
2022-11-04 14:57:44 -0500

    updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var appRouter = require('./routes/Cap');
 var gridbuildRouter = require('./routes/gridbuild');
