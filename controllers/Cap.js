a3dc0f401bb3ba4f13566bb7e5350f40153c6abd
saipuneet <saipuneethpogula7@gmail.com>
2022-11-16 18:12:20 -0600

    updated

--- controllers/Cap.js
+++ controllers/Cap.js
@@ -75,7 +75,7 @@ exports.Cap_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
-   // Handle Costume create on POST.
+   // Handle Cap create on POST.
 exports.Cap_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Cap();
@@ -91,4 +91,73 @@ exports.Cap_create_post = async function(req, res) {
     res.status(500);
     res.send(`{"error": ${err}}`);
     }
-}
+};
+
+// Handle Cap delete on DELETE.
+exports.Cap_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Cap.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
+   // Handle a show one view with id specified by query
+exports.Cap_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Cap.findById( req.query.id)
+    res.render('Capdetail',
+   { title: 'Cap Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for creating a Cap.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Cap_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Capcreate', { title: 'Cap Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a Cap.
+// query provides the id
+exports.Cap_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Cap.findById(req.query.id)
+    res.render('Capupdate', { title: 'Cap Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle a delete one view with id from query
+exports.Cap_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Cap.findById(req.query.id)
+    res.render('Capdelete', { title: 'Cap Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/Cap.js
+++ routes/Cap.js
@@ -1,25 +1,28 @@
 var express = require('express');
-
 var router = express.Router();
 
-
-
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
-
   res.render('Cap', { title: 'Search Results Cap' });
-
 });
 
 var express = require('express');
-
 const Cap_controllers= require('../controllers/Cap');
-
 var router = express.Router();
 
-/* GET costumes */
-
+/* GET Caps */
 router.get('/', Cap_controllers.Cap_view_all_Page );
-
 module.exports = router;
+
+/* GET detail Cap page */
+router.get('/detail', Cap_controllers.Cap_view_one_Page);
+
+/* GET create Cap page */
+router.get('/create', Cap_controllers.Cap_create_Page);
+
+/* GET create update page */
+router.get('/update', Cap_controllers.Cap_update_Page);
+
+/* GET delete Cap page */
+router.get('/delete', Cap_controllers.Cap_delete_Page);
+
