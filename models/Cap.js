61b08779884056c35ac78bdb796751046f8ff484
saipuneet <saipuneethpogula7@gmail.com>
2022-11-30 14:05:25 -0600

    upadated

--- models/Cap.js
+++ models/Cap.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const CapSchema = mongoose.Schema({
-    Cap_Name: {type: String,required: [true, 'Name of the shoe cannot be empty']},
+    Cap_Name: {type: String,required: [true, 'Name of the Cap cannot be empty']},
     Cap_Company: {type: String,required: [true,'Company of the Cap cannot be empty']},
     Cap_Size:{type: String,required:[true,'Size of the Cap cannot be empty']},
     Cap_Rating:{type: String,required:[true,'Rating of the Cap cannot be empty']}
